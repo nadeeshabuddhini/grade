d0d17f3e81845553bbdc2358f9602fdb8847f362
N_Buddhini <39161490+nadeeshabuddhini@users.noreply.github.com>
2019-10-24 22:52:34 +0530

    Add files via upload

---
+++ grade.dart
@@ -0,0 +1,15 @@
+void main(){
+  var x=78;
+  if(x>=75 && x<100){
+    print("A");
+  }else if(x>=65 && x<75){
+    print("B");
+  }else if(x>=35 && x<55){
+    print("C");
+  }else if(x>=0 && x<35){
+    print("F");
+  }else{
+    print("Invalid marks.please try again");
+  }
+}
+
